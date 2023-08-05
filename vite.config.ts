f2250e68f297d270acea448e55c54c312228db5d
D_arh <dimonarhipon@mail.ru>
2023-08-05 20:08:25 +0300

    'upversion'

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
+/// <reference types='vitesvite-plugint' />
+/// <reference types='vite/client' />
 
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
-import { createSvgIconsPlugin } from "vite-plugin-svg-icons"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+// import VitePluginSVGSpritemap from '@spiriit/vite-plugin-svg-spritemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fancy-auth-form/",
+  base: '/fancy-auth-form/',
   plugins: [
     react(),
+		// VitePluginSVGSpritemap('./src/icons/*.svg', {
+    //   styles: 'src/scss/spritemap.scss'
+    // })
     createSvgIconsPlugin({
-      iconDirs: [ path.resolve(process.cwd(), "public/icons") ],
-      symbolId: "icon-[dir]-[name]",
-      customDomId: "svg-sprite",
+      iconDirs: [ path.resolve(process.cwd(), 'public/icons') ],
+      symbolId: 'icon-[dir]-[name]',
+      customDomId: 'svg-sprite',
       svgoOptions: {
         plugins: [
           {
-            name: "convertColors",
+            name: 'convertColors',
             params: {
               currentColor: true
             }
@@ -30,12 +34,13 @@ export default defineConfig({
   css: {
     devSourcemap: true,
   },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-    // you might want to disable it, if you don't have tests that rely on CSS
-    // since parsing CSS is slow
-    css: true,
-  },
-})
+
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  //   setupFiles: './src/test/setup.ts',
+  //   // you might want to disable it, if you don't have tests that rely on CSS
+  //   // since parsing CSS is slow
+  //   css: true,
+  // },
+});
